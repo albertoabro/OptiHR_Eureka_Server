26b76fe7ac1d50e7b1bb1d0332146eb09f8c3662
Alberto Abad <albertoabro@gmail.com>
2024-11-12 12:01:42 +0100

    initial commit

---
+++ src/test/java/com/albertoabadrodriguez/optihr_eurekaserver/OptiHrEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.albertoabadrodriguez.optihr_eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OptiHrEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
